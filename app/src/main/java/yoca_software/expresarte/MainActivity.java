060311a9b530ab370b841e7332f783c4e428ae11
SergioSkateder <sergioskateder@gmail.com>
2016-11-24 22:22:24 -0600

    Initial commit

---
+++ app/src/main/java/yoca_software/expresarte/MainActivity.java
@@ -0,0 +1,13 @@
+package yoca_software.expresarte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
